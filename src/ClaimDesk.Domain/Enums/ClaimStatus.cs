010a1c20fdb951d084f058d679e37acb13dc1acf
Trevoir Williams <trevoir.williams@gmail.com>
2026-06-11 10:39:14 -0500

    Add claim status enum

---
+++ src/ClaimDesk.Domain/Enums/ClaimStatus.cs
@@ -0,0 +1,13 @@
+namespace ClaimDesk.Domain.Enums;
+
+public enum ClaimStatus
+{
+    Draft = 0,
+    Submitted = 1,
+    Assigned = 2,
+    InReview = 3,
+    WaitingOnCustomer = 4,
+    Approved = 5,
+    Denied = 6,
+    Closed = 7
+}
